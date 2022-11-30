a77ae62185e8ca06e582fc30bb95741227299763
a22ismaelpt <ismaelpereztierra@gmail.com>
2022-11-30 13:45:58 +0100

    mostrar clientes en tabla

--- web/urls.py
+++ web/urls.py
@@ -7,4 +7,5 @@ from misitio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ("", views.clientes_list, name="clientes_list"),
+    path ("clientes/new", views.clientes_list, name="clientes_new"),
 ]
